a1ad5b4dd05c4993d5e2195e55be83aebc750d58
KRNGDev <161486725+KRNGDev@users.noreply.github.com>
2024-03-15 20:41:34 +0100

    Update

--- Assets/_GameAssets/Scripts/SpawnerManager.cs
+++ Assets/_GameAssets/Scripts/SpawnerManager.cs
@@ -9,17 +9,34 @@ public class SpawnerManager : MonoBehaviour
 {
     public GameObject[] enemigos;
     public Transform[] spawners;
+    private Collider[] collidersHijos;
     public float tiempoEspera = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        collidersHijos = GetComponentsInChildren<Collider>();
+
         InvokeRepeating("SpawnerEnemigos", tiempoEspera, tiempoEspera);
         //StartCoroutine(SpawnerEnemigos());
+    }
+    void AlgienDentro()
+    {
+        foreach (Collider c in collidersHijos)
+        {
+            OnTriggerEnter(c);
+
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
     }
 
     private void SpawnerEnemigos()
     {
+
+
         GameObject enemigoAleatoio = enemigos[Random.Range(0, enemigos.Length - 1)];
         Transform spawnRandom = spawners[Random.Range(0, spawners.Length - 1)];
 
